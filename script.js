cd11cc8453f7a03be991047b2d3a206581cec766
Adam Measom <adameasom@mail.com>
2023-08-03 00:54:17 +0900

    Skills Loading Animation and User Select

--- script.js
+++ script.js
@@ -13,6 +13,23 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 200); // Adding a 200ms delay just for better visual effect, you can adjust this value as desired
 });
 
+
+// Transition for Down Arrow, after About text
+document.addEventListener("DOMContentLoaded", function() {
+  const loadingArrow = document.getElementById("loadingArrow");
+
+  // Set the time (in milliseconds) you want the fade-in transition to take
+  const transitionDuration = 1500;
+
+  setTimeout(function() {
+    loadingArrow.style.transition = `opacity ${transitionDuration}ms, filter ${transitionDuration}ms, transform ${transitionDuration}ms`;
+    loadingArrow.style.opacity = 1;
+    loadingArrow.style.filter = "blur(0)";
+    loadingArrow.style.transform = "translateY(0)"; // Sliding down to its original position
+  }, 1000); // Adding a 800ms delay just for better visual effect, you can adjust this value as desired
+});
+
+
 // Transition for Left Sidebar
 document.addEventListener("DOMContentLoaded", function() {
     const loadingSidebar = document.getElementById("loadingSidebar");
@@ -24,8 +41,9 @@ document.addEventListener("DOMContentLoaded", function() {
       loadingSidebar.style.transition = `opacity ${transitionDuration}ms, transform ${transitionDuration}ms`;
       loadingSidebar.style.opacity = 1;
       loadingSidebar.style.transform = "translateY(0)"; // Sliding up to its original position
-    }, 200); // Adding a 200ms delay just for better visual effect, you can adjust this value as desired
+    }, 2000); // Adding a 2000ms delay just for better visual effect, you can adjust this value as desired
 });
+
 
 // Transition for About Section
 const divElement = document.getElementById('loadingAbout');
@@ -48,8 +66,46 @@ function fadeInDiv(entries, observer) {
 const observer = new IntersectionObserver(fadeInDiv, {
   root: null,
   rootMargin: '0px',
-  threshold: 0.8 // Adjust this threshold value to control when the animation starts
+  threshold: 0.7 // Adjust this threshold value to control when the animation starts
 });
 
 // Start observing the section
 observer.observe(section);
+
+
+// Transition for each skill
+document.addEventListener('DOMContentLoaded', () => {
+  const boxes = document.querySelectorAll('.box');
+  const transitionDuration = 1000; // 1000ms
+
+  function animateBoxes(index) {
+    if (index >= boxes.length) return;
+    const box = boxes[index];
+    box.style.transition = `transform ${transitionDuration}ms, opacity ${transitionDuration / 2}ms`;
+    box.style.transform = 'translateY(0)';
+    box.style.opacity = '1';
+    setTimeout(() => {
+      animateBoxes(index + 1);
+    }, transitionDuration / 2); // Delay for the next animation
+  }
+
+  // Use Intersection Observer to trigger the animation when the section is in the viewport
+  const skillsSection = document.getElementById('skillsBox');
+  const options = {
+    root: null, // Use the viewport as the root
+    rootMargin: '0px',
+    threshold: 0.5, // Trigger when at least 50% of the section is visible
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Start the animation only when the section enters the viewport
+        animateBoxes(0);
+        observer.unobserve(entry.target); // Stop observing once triggered
+      }
+    });
+  }, options);
+
+  observer.observe(skillsSection);
+});
